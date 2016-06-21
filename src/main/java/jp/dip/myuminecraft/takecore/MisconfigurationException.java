555c680b52def5e5cad397723b01563f3d98f79c
Taketsuru <taketsuru11@gmail.com>
2016-06-21 10:46:52 +0900

    Add an utility exception class 'MisconfigurationException'.

---
+++ src/main/java/jp/dip/myuminecraft/takecore/MisconfigurationException.java
@@ -0,0 +1,20 @@
+package jp.dip.myuminecraft.takecore;
+
+public class MisconfigurationException extends Exception {
+    static final long serialVersionUID = 1;
+
+    public MisconfigurationException() {
+    }
+
+    public MisconfigurationException(String message) {
+        super(message);
+    }
+
+    public MisconfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MisconfigurationException(Throwable cause) {
+        super(cause);
+    }
+}
